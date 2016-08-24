9cab159a55ada831a4cd2d26ae9b9f3cc83b10df
KartoshkaD <leshkadolg@gmail.com>
2016-08-24 16:15:12 +0300

    Base layout created.

--- app/src/main/java/com/example/android/testtask/MainActivity.java
+++ app/src/main/java/com/example/android/testtask/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.android.testtask;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Button mFetchBtn;
+    private Button mDetailsBtn;
+    private RecyclerView mUsersRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mFetchBtn = (Button) findViewById(R.id.fetch_btn);
+        mFetchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "Fetch button pressed.");
+            }
+        });
+
+        mDetailsBtn = (Button) findViewById(R.id.details_btn);
+        mDetailsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "Fetch button pressed.");
+            }
+        });
+
+
     }
 }
